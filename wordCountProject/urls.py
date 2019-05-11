7d75239a089f6278b73678561212f07c629c0d2d
kmlee95 <kmlee95@naver.com>
2019-05-11 18:37:17 +0900

    word count (기본)

--- wordCountProject/urls.py
+++ wordCountProject/urls.py
@@ -20,4 +20,7 @@ import wordcount.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', wordcount.views.home, name="home"),
+    path('about/', wordcount.views.about, name="about"),
+    path('count/', wordcount.views.count, name="count"),
+
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -4,4 +4,24 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'wordcount/home.html')
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def count(request):
+    full_text = request.GET['fulltext']  # fulltext로부터 데이터 얻어옴
+    word_list = full_text.split()  # 단어를 나누기 위해.
+    word_dictionary = {}
+
+    for word in word_list:
+        if word in word_dictionary:
+            # Increase
+            word_dictionary[word] += 1
+        else:
+            # add to the dictionary
+            word_dictionary[word] = 1
+
+    return render(request, 'count.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()})
